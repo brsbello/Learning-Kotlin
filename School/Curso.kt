ecc76b8fddfdd1780e292dac0356a7914f64b8e4
brsbello <Br!753159>
2021-11-23 21:15:54 -0300

    Implementação dos métodos matricularAluno e desmatricularAluno nos Cursos.

--- School/Curso.kt
+++ School/Curso.kt
@@ -5,4 +5,31 @@ class Curso(
     var professorAdjunto: ProfessorAdjunto? = null,
     val maxAlunos: Int,
     var listaAlunos: MutableList<Int?> = mutableListOf()
-)
+) {
+    fun matricularAluno(umAluno: Aluno): Boolean {
+        var podeAdicionar = false
+        if (listaAlunos.size >= maxAlunos) {
+            println("Não é possível matricular ${umAluno.nome} no curso de $nomeDoCurso")
+            return podeAdicionar
+        } else {
+            if (listaAlunos.contains(umAluno.codigoAluno)) {
+                println("${umAluno.nome} já está matriculado no curso de $nomeDoCurso")
+                return podeAdicionar
+            } else {
+                listaAlunos.add(umAluno.codigoAluno)
+                println("${umAluno.nome} foi matriculado no curso de $nomeDoCurso")
+                podeAdicionar = true
+                return podeAdicionar
+            }
+        }
+    }
+
+    fun desmatricularAluno(umAluno: Aluno) {
+        if (listaAlunos.contains(umAluno.codigoAluno)) {
+            listaAlunos.remove(umAluno.codigoAluno)
+            println("${umAluno.nome} foi desmatriculado do curso de $nomeDoCurso")
+        } else {
+            println("${umAluno.nome} não está matriculado no curso de $nomeDoCurso")
+        }
+    }
+}
